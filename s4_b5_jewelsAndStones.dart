fbf7c2df774fdfca40135f66373098654733ce5f
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2023-01-04 18:33:07 +1000

    Add files via upload

---
+++ s4_b5_jewelsAndStones.dart
@@ -0,0 +1,36 @@
+//Bài 5: https://leetcode.com/problems/jewels-and-stones/
+
+class Solution {
+  int numJewelsInStones(String jewels, String stones) {
+                int times=0;
+        for(int i=0;i<stones.length;i++)
+        {
+                  for(int j=0;j<jewels.length;j++)
+        {
+          
+                    if(stones[i] == jewels[j])
+                    {
+                      times++;
+                    }
+        }
+        }
+    return times;
+  }
+}
+
+// class Solution {
+//   int numJewelsInStones(String jewels, String stones) {
+//         int times=0;
+//         List<String> jewelsStr= jewels.split('');
+//         List<String> stonesStr= stones.split('');
+//         stonesStr.forEach((item){
+//           jewelsStr.forEach((word){
+//                                 if(item == word)
+//                     {
+//                       times++;
+//                     }
+//           });
+//         });
+//     return times;
+//   }
+// }
