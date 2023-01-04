a7a4e55cd117ba9bfbc3b369663709c9377f5d1d
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2023-01-05 00:02:16 +1000

    Update s4_b5_jewelsAndStones.dart

--- s4_b5_jewelsAndStones.dart
+++ s4_b5_jewelsAndStones.dart
@@ -6,8 +6,7 @@ class Solution {
         for(int i=0;i<stones.length;i++)
         {
                   for(int j=0;j<jewels.length;j++)
-        {
-          
+        {    
                     if(stones[i] == jewels[j])
                     {
                       times++;
